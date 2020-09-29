56ad902ea9cbe5cb4f118fb04e648becf077bb17
AdrianDe07 <delaasuncionbuelvasadrian@gmail.com>
2020-09-29 00:31:04 -0500

    updating

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './css/styles.css';
 
 function Home() {
-
-    
-    return ( 
+  return (
       <div className="container">
-          <div class="swiper-container">
-            <div class="swiper-wrapper">
-            <div class="swiper-slide" style="background-image:url(http://lorempixel.com/600/600/nature/1)"></div>
-            <div class="swiper-slide" style="background-image:url(http://lorempixel.com/600/600/nature/2)"></div>
-            <div class="swiper-slide" style="background-image:url(http://lorempixel.com/600/600/nature/3)"></div>
-        </div>
-        <div class="swiper-pagination"></div>
-        </div>
+          <div className="box-slides">
+            <div className="slides">
+    
+            </div>
+          </div>
       </div>
     );
-    
-    }
-
+}
 
 export default Home;

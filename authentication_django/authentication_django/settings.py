54474c72e385339adc1987b666a4f5dad9f932b4
Esteban1891 <esteban.delahoz15@gmail.com>
2021-04-28 00:33:51 -0500

    add email reset passws also verification email

--- authentication_django/authentication_django/settings.py
+++ authentication_django/authentication_django/settings.py
@@ -165,6 +165,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # borrar si hay error
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS=True 
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+
+APPLICATION_EMAIL = 'Recipeat<esteban.delahoz15@gmail.com>'
+DEFAULT_FROM_EMAIL = 'Recipeat<esteban.delahoz15@gmail.com>'
+EMAIL_HOST_PASSWORD= 'ghssoowbyvwzmerl'#os.environ.get('EMAIL_HOST_PASSWORD')
+
+EMAIL_HOST_USER= 'esteban.delahoz15@gmail.com'#os.environ.get('EMAIL_HOST_USER')
 django_heroku.settings(locals()) # borrar si hay error
